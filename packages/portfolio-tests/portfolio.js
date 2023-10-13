3d9687720ceb0ca8bace76c97821d94f4816a9c4
Kyle Reyes <73966157+Proefey@users.noreply.github.com>
2023-10-13 09:46:22 -0700

    Refactored and Added More Tests

--- packages/portfolio-tests/portfolio.js
+++ packages/portfolio-tests/portfolio.js
@@ -1,31 +1,47 @@
 var shares = [];
 
+//Returns full list of shares
 function getShares(){
 	return shares;
 }
 
+//Returns number of shares in total
+function totalShares(){
+	return shares.length;
+}
+
+//Returns a true if no shares are owned
 function isEmpty(){
 	return shares.length == 0;
 }
 
-//For testing Purposes Only
+//Sets list of shares to a new list, used for testing purposes only
 function setShares(newShares){
 	shares = newShares;
 }
 
+//Returns a list of unique shares
 function uniqueShares(){
 	var unique = [];
 	shares.forEach((share) =>{
 		if(!unique.includes(share) && share.length > 0) unique.push(share)
 	});
-	return unique.length;
+	return unique;
 }
 
+//Returns a count of unique shares
+function countUnique(){
+	return uniqueShares().length
+}
+
+//Adds shares to the shares list
 function addShares(share, count){
+	if(share.length < 1) return;
 	for(let i = 0; i < count; i++) shares.push(share);
 	return;
 }
 
+//Sells shares from the shares list, throws ShareSaleException if invalid arguements
 function sellShares(sellshare, count){
 	const totalcount = countShares(sellshare);
 	if(totalcount < count) {
@@ -51,4 +67,5 @@ function countShares(countshare){
 }
 
 export default {getShares, isEmpty, setShares, 
-	uniqueShares, addShares, sellShares, countShares}
+	totalShares, uniqueShares, addShares, 
+	sellShares, countUnique, countShares}
--- packages/portfolio-tests/portfolio.test.js
+++ packages/portfolio-tests/portfolio.test.js
@@ -3,28 +3,29 @@ import mut from './portfolio.js';
 //Question #1
 test('Testing Create Portfolio', () => {
 	const init_length = 0;
-	const gotShares = mut.getShares();
-	expect(gotShares.length).toBe(init_length);
+	expect(mut.totalShares()).toBe(init_length);
 });
 
 //Question #2
 test('Testing Is Empty', () => {
 	expect(mut.isEmpty()).toBeTruthy();
+
 	mut.setShares(["AMC"]);
 	expect(mut.isEmpty()).toBeFalsy();
 });
 
 //Question #3
 test('Testing Unique Shares', () => {
-	expect(mut.uniqueShares()).toBe(1);
+	mut.setShares([]);
+	expect(mut.countUnique()).toBe(0);
+
+	mut.setShares(["AMC"]);
+	expect(mut.countUnique()).toBe(1);
+
 	mut.setShares(["GME", "GME", "GME", "GME", "GME", "RBLX" 
 		,"RBLX" ,"RBLX" ,"RBLX" ,"RBLX" ,"RBLX" ,"RBLX" ,"RBLX" 
 		,"RBLX" ,"RBLX" ]);
-	expect(mut.uniqueShares()).toBe(2);
-	mut.setShares([]);
-	expect(mut.uniqueShares()).toBe(0);
-	mut.setShares([""]);
-	expect(mut.uniqueShares()).toBe(0);
+	expect(mut.countUnique()).toBe(2);
 });
 
 //Question #4
@@ -32,29 +33,38 @@ test('Testing Adding Shares', () => {
 	mut.setShares([]);
 	mut.addShares("AMC", 0);
 	expect(mut.isEmpty()).toBeTruthy();
+
 	const sharecount = 2;
 	mut.addShares("AMC", sharecount);
-	const gotShares = mut.getShares();
-	expect(gotShares.length).toBe(sharecount);
-	expect(mut.uniqueShares()).toBe(1);
+	expect(mut.totalShares()).toBe(sharecount);
+	expect(mut.uniqueShares()).toContain("AMC");
+
+	//Should not add due to being an invalid token (Does not throw error)
+	mut.addShares("", 1)
+	expect(mut.totalShares()).toBe(sharecount);
 });
 
 //Question #5
 test('Testing Selling Shares', () => {
 	mut.setShares([]);
-	mut.addShares("AMC", 5);
-	mut.addShares("RBLX", 5);
-	var gotShares = mut.getShares();
-	expect(gotShares.length).toBe(10);
+	//Interspercing Shares in the array
+	mut.addShares("AMC", 1);
+	mut.addShares("RBLX", 1);
+	mut.addShares("AMC", 3);
+	mut.addShares("RBLX", 2);
+	mut.addShares("AMC", 1);
+	mut.addShares("RBLX", 2);
+	expect(mut.totalShares()).toBe(10);
+
 	mut.sellShares("AMC", 0);
-	gotShares = mut.getShares();
-	expect(gotShares.length).toBe(10);
+	expect(mut.totalShares()).toBe(10);
+
 	mut.sellShares("AMC", 3);
-	gotShares = mut.getShares();
-	expect(gotShares.length).toBe(7);
+	expect(mut.totalShares()).toBe(7);
+
 	mut.sellShares("AMC", 2);
-	gotShares = mut.getShares();
-	expect(gotShares.length).toBe(5);
+	expect(mut.totalShares()).toBe(5);
+
 	mut.sellShares("RBLX", 5);
 	expect(mut.isEmpty()).toBeTruthy();
 });
@@ -69,15 +79,28 @@ test('Testing Count Shares', () => {
 	expect(mut.countShares("RBLX")).toBe(7);
 });
 
-//Question #7: The way the portfolio is implemented
-//It is impossible for a Symbol to be in the portfolio
-//without being currently owned at least once
+//Question #7:
+test('Testing Keep Only Owned Shares', () => {
+	mut.setShares([]);
+	mut.addShares("AMC", 2);
+	mut.addShares("RBLX", 2);
+	expect(mut.uniqueShares()).toContain("AMC");
+	expect(mut.uniqueShares()).toContain("RBLX");
+
+	mut.sellShares("AMC", 2);
+	expect(mut.uniqueShares()).toContain("RBLX");
+	expect(mut.uniqueShares().includes("AMC")).toBeFalsy();
+
+});
 
 //Question #8: Testing Throwing Error
 test('Testing Selling Shares Error', () => {
+	mut.setShares([]);
+	mut.addShares("AMC", 1);
+	mut.addShares("RBLX", 2);
 	function overSellShares(sellShare, count){
 		mut.sellShares(sellShare, count);
 	}
-	expect(() => {overSellShares("AMC", 6)}).toThrow('ShareSaleException');
-	expect(() => {overSellShares("GME", 6)}).toThrow('ShareSaleException');
+	expect(() => {overSellShares("AMC", 2)}).toThrow('ShareSaleException');
+	expect(() => {overSellShares("GME", 1)}).toThrow('ShareSaleException');
 });
