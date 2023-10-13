6e2f775a2e0545b3ce5cf50b628c1391d8859c8c
Kyle Reyes <73966157+Proefey@users.noreply.github.com>
2023-10-13 01:21:17 -0700

    Question #1

---
+++ packages/portfolio-tests/portfolio.js
@@ -0,0 +1,12 @@
+const tickerSymbols = [];
+const shares = [];
+
+function getTickerSymbols(){
+	return tickerSymbols;
+}
+
+function getShares(){
+	return shares;
+}
+
+export default {getTickerSymbols, getShares}
--- packages/portfolio-tests/portfolio.test.js
+++ packages/portfolio-tests/portfolio.test.js
@@ -1,1 +1,9 @@
-import mut from './module.js';
+import mut from './portfolio.js';
+
+test('Testing Create Portfolio', () => {
+	const init_length = 0;
+	const gotTickerSymbols = mut.getTickerSymbols();
+	const gotShares = mut.getShares();
+	expect(gotTickerSymbols.length).toBe(init_length);
+	expect(gotShares.length).toBe(init_length);
+});
