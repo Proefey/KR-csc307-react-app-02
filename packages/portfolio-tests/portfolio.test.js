a53aae59e1dd3f3ff0ac893008de25b9120f5d69
Kyle Reyes <73966157+Proefey@users.noreply.github.com>
2023-10-16 00:43:42 -0700

    Refactored Tests

--- packages/portfolio-tests/portfolio.test.js
+++ packages/portfolio-tests/portfolio.test.js
@@ -2,105 +2,154 @@ import mut from './portfolio.js';
 
 //Question #1
 test('Testing Create Portfolio', () => {
-	const init_length = 0;
-	expect(mut.totalShares()).toBe(init_length);
+	expect(mut.totalShares()).toBe(0);
 });
 
 //Question #2
-test('Testing Is Empty', () => {
+test('Testing Creation Is Empty', () => {
 	expect(mut.isEmpty()).toBeTruthy();
+});
 
+test('Testing Creation Can Be Non-Empty', () => {
 	mut.setShares(["AMC"]);
+
 	expect(mut.isEmpty()).toBeFalsy();
 });
 
 //Question #3
-test('Testing Unique Shares', () => {
+test('Testing Unique Shares With Empty Portfolio', () => {
 	mut.setShares([]);
+
 	expect(mut.countUnique()).toBe(0);
+});
 
+test('Testing Unique Shares With Single Item', () => {
 	mut.setShares(["AMC"]);
-	expect(mut.countUnique()).toBe(1);
 
+	expect(mut.countUnique()).toBe(1);
+});
+
+test('Testing Unique Shares With Multiple Items', () => {
 	mut.setShares(["GME", "GME", "GME", "GME", "GME", "RBLX" 
 		,"RBLX" ,"RBLX" ,"RBLX" ,"RBLX" ,"RBLX" ,"RBLX" ,"RBLX" 
 		,"RBLX" ,"RBLX" ]);
+
 	expect(mut.countUnique()).toBe(2);
 });
 
 //Question #4
-test('Testing Adding Shares', () => {
+test('Testing Adding No Shares', () => {
 	mut.setShares([]);
 	mut.addShares("AMC", 0);
-	expect(mut.isEmpty()).toBeTruthy();
 
+	expect(mut.totalShares()).toBe(0);
+});
+
+test('Testing Adding Some Shares', () => {
 	const sharecount = 2;
+	mut.setShares([]);
 	mut.addShares("AMC", sharecount);
-	expect(mut.totalShares()).toBe(sharecount);
-	expect(mut.uniqueShares()).toContain("AMC");
 
-	//Should not add due to being an invalid token (Does not throw error)
-	mut.addShares("", 1)
 	expect(mut.totalShares()).toBe(sharecount);
+	expect(mut.getShares()).toContain("AMC");
+});
+
+test('Testing Adding An Invalid Share', () => {
+	mut.setShares([]);
+	mut.addShares("", 1);
+
+	expect(mut.totalShares()).toBe(0);
 });
 
 //Question #5
-test('Testing Selling Shares', () => {
-	mut.setShares([]);
-	//Interspercing Shares in the array
-	mut.addShares("AMC", 1);
-	mut.addShares("RBLX", 1);
-	mut.addShares("AMC", 3);
-	mut.addShares("RBLX", 2);
-	mut.addShares("AMC", 1);
-	mut.addShares("RBLX", 2);
-	expect(mut.totalShares()).toBe(10);
-
+test('Testing Selling No Shares', () => {
+	mut.setShares(["AMC", "RBLX", "AMC", "AMC", 
+		"AMC", "RBLX", "RBLX", "AMC", 
+		"RBLX", "RBLX"]);
 	mut.sellShares("AMC", 0);
+
 	expect(mut.totalShares()).toBe(10);
+});
 
+test('Testing Selling Some Shares', () => {
+	mut.setShares(["AMC", "RBLX", "AMC", "AMC", 
+		"AMC", "RBLX", "RBLX", "AMC", 
+		"RBLX", "RBLX"]);
 	mut.sellShares("AMC", 3);
+
 	expect(mut.totalShares()).toBe(7);
+});
 
-	mut.sellShares("AMC", 2);
-	expect(mut.totalShares()).toBe(5);
-
+test('Testing Selling All Shares', () => {
+	mut.setShares(["AMC", "RBLX", "AMC", "AMC", 
+		"AMC", "RBLX", "RBLX", "AMC", 
+		"RBLX", "RBLX"]);
+	mut.sellShares("AMC", 5);
 	mut.sellShares("RBLX", 5);
-	expect(mut.isEmpty()).toBeTruthy();
+
+	expect(mut.totalShares()).toBe(0);
 });
 
 //Question #6
-test('Testing Count Shares', () => {
+test('Testing Count Shares Where There Are None', () => {
 	mut.setShares([]);
 	mut.addShares("AMC", 5);
-	mut.addShares("RBLX", 7);
+
 	expect(mut.countShares("GME")).toBe(0);
+});
+
+test('Testing Count Shares Where There Are Some', () => {
+	mut.setShares([]);
+	mut.addShares("AMC", 5);
+	mut.addShares("RBLX", 5);
+
 	expect(mut.countShares("AMC")).toBe(5);
-	expect(mut.countShares("RBLX")).toBe(7);
 });
 
 //Question #7:
-test('Testing Keep Only Owned Shares', () => {
+test('Testing Keeps Partially Owned Shares', () => {
 	mut.setShares([]);
 	mut.addShares("AMC", 2);
 	mut.addShares("RBLX", 2);
-	expect(mut.uniqueShares()).toContain("AMC");
-	expect(mut.uniqueShares()).toContain("RBLX");
+
+	mut.sellShares("AMC", 1);
+
+	expect(mut.getShares()).toContain("AMC");
+	expect(mut.getShares()).toContain("RBLX");
+
+});
+
+test('Testing Does Not Keep Sold Shares', () => {
+	mut.setShares([]);
+	mut.addShares("AMC", 2);
+	mut.addShares("RBLX", 2);
 
 	mut.sellShares("AMC", 2);
-	expect(mut.uniqueShares()).toContain("RBLX");
-	expect(mut.uniqueShares().includes("AMC")).toBeFalsy();
+
+	expect(mut.getShares()).toContain("RBLX");
+	expect(mut.getShares().includes("AMC")).toBeFalsy();
 
 });
 
 //Question #8: Testing Throwing Error
-test('Testing Selling Shares Error', () => {
+test('Testing Selling Shares Error If Selling A Non-Owned Stock', () => {
 	mut.setShares([]);
 	mut.addShares("AMC", 1);
 	mut.addShares("RBLX", 2);
 	function overSellShares(sellShare, count){
 		mut.sellShares(sellShare, count);
 	}
-	expect(() => {overSellShares("AMC", 2)}).toThrow('ShareSaleException');
+
 	expect(() => {overSellShares("GME", 1)}).toThrow('ShareSaleException');
+});
+
+test('Testing Selling Shares Error If Selling Too Much Of A Stock', () => {
+	mut.setShares([]);
+	mut.addShares("AMC", 1);
+	mut.addShares("RBLX", 2);
+	function overSellShares(sellShare, count){
+		mut.sellShares(sellShare, count);
+	}
+
+	expect(() => {overSellShares("AMC", 2)}).toThrow('ShareSaleException');
 });
